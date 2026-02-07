bbb8d31397764514a2ec7bfa169c4c6f6de321a4
saurabh pandey <saurabhpandey5857@gmail.com>
2026-02-07 16:30:21 +0530

    this is sorting file

--- Arrayacc.java
+++ Arrayacc.java
@@ -1,5 +1,4 @@
 
-// import com.sun.source.doctree.ReturnTree;
 
 //import java.util.*;
 // public class Arrayacc{
@@ -66,61 +65,156 @@
 // //}
 //}
 //WRITE A PROGRAM TO PRINT THE LARGEST NUMBER
-public class Arrayacc{
-  public static void getlergest(int number[]){
-    int largest = Integer.MIN_VALUE;
-    int secondlargest = Integer.MIN_VALUE;
-    for (int i = 0; i < number.length; i++) {
-      if (largest<number[i]) {
-          largest=number[i];
+// public class Arrayacc{
+//   public static void getlergest(int number[]){
+//     int largest = Integer.MIN_VALUE;
+//     int secondlargest = Integer.MIN_VALUE;
+//     for (int i = 0; i < number.length; i++) {
+//       if (largest<number[i]) {
+//           largest=number[i];
           
-      }
+//       }
       
 
-     // if(number[i]>largest)
-       // System.out.println("the largest number is"+number[i]);
-    }
-    System.out.println("the largest number is"+largest);
+//      // if(number[i]>largest)
+//        // System.out.println("the largest number is"+number[i]);
+//     }
+//     System.out.println("the largest number is"+largest);
           
-    for (int i = 0; i < number.length; i++) {
-           if (number[i]==largest) {
-              continue;
-          } 
-          if(secondlargest<number[i]){
-          secondlargest=number[i];
-          //System.out.println("the second largest number is"+secondlargest);
+//     for (int i = 0; i < number.length; i++) {
+//            if (number[i]==largest) {
+//               continue;
+//           } 
+//           if(secondlargest<number[i]){
+//           secondlargest=number[i];
+//           //System.out.println("the second largest number is"+secondlargest);
           
-      }
-          }
-          System.out.println("the second largest number is"+secondlargest);
-          }
-    // return largest;
-  public static void main(String[] args) {
-    int number[]={1,2,3,78,5,6,49,79};
-    // int a =getlergest(number);
-    // System.out.println("the largest number is"+a);
-    getlergest(number);
-  }
-}
-// //WRITE A PRORAM TO BINARY SEARCH
+//       }
+//           }
+//           System.out.println("the second largest number is"+secondlargest);
+//           }
+//     // return largest;
+//   public static void main(String[] args) {
+//     int number[]={1,2,3,78,5,6,49,79};
+//     // int a =getlergest(number);
+//     // System.out.println("the largest number is"+a);
+//     getlergest(number);
+//   }
+// }
+// // //WRITE A PRORAM TO BINARY SEARCH
+// // public class Arrayacc{
+// //   public static void binarysearch(int number[],int key){
+// //     int start =0;
+// //     int end= number.length-1;
+// //     while (start<=end) { 
+// //         int mid=(start+end)/2;
+// //         if (number[mid]==key) {
+// //             System.out.println(mid);
+// //         }if (number[mid]<key) {
+// //             start = mid+1;
+// //         }else{
+// //           end = mid-1;
+// //         }
+// //     }
+// //   }
+// //   public static void main(String[] args) {
+// //       int number[]={2,4,6,8,10,12,14};
+// //       int key=4;
+// //       binarysearch(number, key);
+// //   }
+// // }
+
+//A PROGRAM TO REVERSE OF A NUMBER
 // public class Arrayacc{
-//   public static void binarysearch(int number[],int key){
-//     int start =0;
-//     int end= number.length-1;
-//     while (start<=end) { 
-//         int mid=(start+end)/2;
-//         if (number[mid]==key) {
-//             System.out.println(mid);
-//         }if (number[mid]<key) {
-//             start = mid+1;
-//         }else{
-//           end = mid-1;
-//         }
+//   public static void reverse(int number[]){
+//     int frist =0, last=number.length-1;
+//     while (frist<last) { 
+//       // swap number  karne ka code hai ye
+//       int temp=number[last];
+//       number[last]=number[frist];
+//       number[frist]=temp;
+//       frist++;
+//       last--;
+        
 //     }
 //   }
 //   public static void main(String[] args) {
-//       int number[]={2,4,6,8,10,12,14};
-//       int key=4;
-//       binarysearch(number, key);
+//       int number[]={2,4,6,8,10};
+//       reverse(number);
+//           for (int i = 0; i <number.length; i++) {
+//               System.out.print( number[i]+" ");
+//           }
+//           System.out.println(" ");
+//       }
+//   }
+
+//WRITE A PROGRAM TO PRINT A PAIR IN AN Array
+// public class Arrayacc{
+//   public static void main(String[] args) {
+//       int number []={1,2,3,4,5};
+//       int tp =0;
+//       for (int i = 0; i < number.length; i++) {
+//         int current=number[i];//matlab  current = 1 position me hai
+        
+//         for(int j=i+1;j<number.length;j++){
+//           System.out.print("(" + current +" ," + number[j] + " ");
+//           tp++;// matlab total pairs ++ ho jaye
+//       }
+//       System.out.println(" ");
+//       }
+//       System.out.println("total pairs="+ tp);
+//   }}
+
+//WRITE A PROGRAM TO PRINT SUB TYPE OF ARRAY
+// public class Arrayacc{
+//   public static void subtype(int number[]){
+//     int ts=0;
+//     for (int i =0; i <=number.length; i++) {// ye loop start find karne ke liye 
+//       int start=i;
+//     for (int j = i; j <=number.length; j++) {//ye loop end find karne ke liye array me
+//       int end =j;
+//       for (int k = start; k <= end; k++) {// value print karane ke liye
+//         System.out.print(number[k]+" ");
+//         //  System.out.println("the total number of the sub array is"+number[k]);
+//       }
+//       ts++;
+//       System.out.println();
+//     }
+// System.out.println("total subarrays = " + ts);
+//     }
+//   }
+//   public static void main(String[] args) {
+//       int number[]={2,4,6,8,10};
+//       subtype(number);
 //   }
 // }
+
+// public class Arrayacc{
+//   public static void main(String[] args) {
+//       int arr[]={2,4,6,8,10};
+//       int target=10;
+//       other:
+//       for (int i = 0; i < arr.length; i++) {
+//         for (int j = i+1; j < arr.length; j++){
+//           if (arr[i]+  arr[j] ==target){
+//             System.out.println("number of one is"+ arr[i] +" "+ "numberof" +arr[j]+" ");
+//             break other;
+//           }
+//         }
+//       }
+//   }
+//}
+public class Arrayacc{
+public static void main(String[] args) {
+    int arr[]={5,4,1,3,2};
+    for (int i = 1; i < arr.length; i++) {
+        int curr = arr[i];
+        int prev = i-1;
+        while(prev >=0 && arr[prev]>curr){
+          arr[prev+1]=arr[prev];
+          prev--;
+        }
+        arr[prev+1]=arr[prev];
+    }
+}
+}
--- sorting.java
+++ sorting.java
@@ -1,1 +1,70 @@
-.
+// public class sorting{
+//     public static void bubblesort(int arr[]){
+//         for(int i=0;i<arr.length-1;i++){
+//             for(int j=0;j<arr.length-1-i;j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int temp=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=temp;
+//                 }
+//             }
+//         }
+//     }
+
+// public static void printarray(int arr[]){
+//     for(int i=0;i<arr.length;i++){
+//         System.out.print(arr[i]+" ");
+//     }
+//     System.out.println();
+// }
+// public static void main(String args[]){
+//     int arr[]={5,4,1,3,2};
+//     bubblesort(arr);
+//     printarray(arr);
+//     }
+// }
+
+//SORTING USING INBUILT FUNCATION
+`// import java.util.Arrays;
+// public class sorting{
+//     public static void main(String args[]){
+//         int arr[]={5,4,1,3,2};
+//         Arrays.sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+//SELECTION SORT
+public class sorting{
+public static void selectionsort(int arr[]){
+    for(int i=0;i<arr.length-1;i++){
+        int minpos=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[minpos]>arr[j]){
+                minpos=j;
+
+
+            }
+        }
+        int temp=arr[minpos];
+        arr[minpos]=arr[i];
+        arr[i]=temp;
+        // for (int k = 0; k < arr.length; k++) { e 
+        //     System.out.print(arr[k] + " ");
+        // }
+        // System.out.println();
+        // yaha pe loop lagane se puri swaping one by one dikhegi
+    }
+    for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+        System.out.println();
+    
+}
+public static void main(String args[]){
+    int arr[]={5,4,1,3,2};
+    selectionsort(arr);
+   // for(int i=0;i<arr.length;i++){
+     //   System.out.print(arr[i]+" ");
+    }
+}
+
